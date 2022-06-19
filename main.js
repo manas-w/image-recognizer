a56776a4bf3f118fe014209c7c9ceacd2de286c7
manas-w <103320246+manas-w@users.noreply.github.com>
2022-06-19 12:40:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+webcam.set({
+    widht:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src ="'+data_uri+'"'
+    });
+}
+
+console.log('m15 version:', m15.version);
+
+classifier = m15.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+
+
+
+function modelLoaded() {
+
+    console.log('Model Loaded!');
+}
+
+
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, results) {
+if (error) {
+    console.error(error);
+}   else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+}
+    }
